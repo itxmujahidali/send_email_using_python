69a642c1ec5b837ddb91add7ea1c826975b0299c
Mujahid Ali <46293412+itxmujahidali@users.noreply.github.com>
2022-03-03 23:36:05 -0800

    add random number generator.
    add some random number and send via email to use in forget password and in verification email.

--- sending_email.py
+++ sending_email.py
@@ -1,20 +1,28 @@
 import smtplib
+from random import randint
+
 gmail_user = "example@gmail.com"
-gmail_pwd = "password1122334455"
+gmail_pwd = "enteryourpasswordhere"
 TO = 'example@gmail.com'
-SUBJECT = "Testing sending using gmail"
-TEXT = "Testing sending mail using gmail servers"
+SUBJECT = "SIS Recovery Code"
+TEXT = "Your recovery code is: "
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.ehlo()
 server.starttls()
 server.login(gmail_user, gmail_pwd)
+
+#generating random numbers
+number_generate = randint(100001,999999)
+
 BODY = '\r\n'.join(['To: %s' % TO,
         'From: %s' % gmail_user,
         'Subject: %s' % SUBJECT,
-        '', TEXT])
+        '', TEXT+str(number_generate)])
 
 server.sendmail(gmail_user, [TO], BODY)
-print ('email sent')
 
-''' Before send this email, Make sure
+print (f'Email has been send! \nRecovery code is: {number_generate}')
+
+''' -------- Note ----------
+    Before send this email, Make sure
     you turn ON your gmail setting "Less app secure". '''
